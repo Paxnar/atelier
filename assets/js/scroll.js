b8f9a0425e74ad79c24c8f2f0b3946933e1d61b0
PavelUkilevich <ukilevich@yandex.eu>
2023-01-21 01:19:28 +0300

    stuff

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -33,81 +33,9 @@ jQuery(function($) {
 
         $(window).scroll(headerFixed);
         headerFixed();
-/*
-        // Scrollspy init
-        new Gumshoe('.js-header .js-scroll', {
-            navClass: 'is-active',
-            offset: function () {
-                return $('.js-header')[0].getBoundingClientRect().height;
-            }
-        });
-
-        $header.find('.js-scroll').click(function () {
-            $html.removeClass('nav-open');
-            setTimeout(function () { $html.css('overflow', 'auto') }, 300)
-        });
-
-        $('.js-nav-trigger').click(function (e) {
+        $('.header-side-menu').click(function (e) {
             e.preventDefault();
             $html.toggleClass('nav-open');
         });
-
-        $(document).click(function (e) {
-           if ($html.hasClass('nav-open') && !$(e.target).closest('.header').length)
-               $html.removeClass('nav-open');
-        })
-*/
     })();
-
-    /*// Scroll to
-    (function () {
-        new SmoothScroll('.js-scroll', {
-            updateURL: false,
-            offset: function (anchor, toggle) {
-                return 70;
-            },
-        });
-    })();
-
-    // Up
-    $('.js-up').click(function (e) {
-        e.preventDefault();
-        $('html,body').animate({scrollTop:0},0);
-    });
-
-    $('.js-replace').widthReplace();
-
-    AOS.init({ once: true, duration: 600, offset: 0, disable: 'mobile' });
-
-
-    // Show more works
-    $('.js-show-works').click(function (e) {
-        e.preventDefault();
-        $(this).hide();
-        $('.js-hidden-works').removeClass('d-none')
-    });
-
-    // Works gallery
-    (function () {
-        $('.js-work').click(function (e) {
-            e.preventDefault();
-            var $this = $(this);
-            var gallery = $this.data('gallery');
-            var els = [];
-
-            $('.js-work[data-gallery="' + gallery + '"]').each(function () {
-               els.push({ src: $(this).attr('href') })
-            });
-
-            $this.lightGallery({
-                dynamic: true,
-                dynamicEl: els,
-                //zoom: true,
-                hideBarsDelay: 999999999,
-                download: false,
-                counter: false,
-                mode: 'lg-soft-zoom'
-            });
-        });
-    })();*/
 });
